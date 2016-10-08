8e170481c4aada20e6293c9a1b3cdc3debd0c1ec
tomcat2088 <tomcat1991@126.com>
2016-10-08 14:39:14 +0800

    Initial Commit

---
+++ ARTech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARTech
+//
+//  Created by wangyang on 2016/10/8.
+//  Copyright © 2016年 wangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARTech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARTech
+//
+//  Created by wangyang on 2016/10/8.
+//  Copyright © 2016年 wangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
